c75dc6a7e43e82e2e464493060c4dddd54370f2a
zichengyu <yuzichengyzc@gmail.com>
2026-04-12 19:22:22 +0800

    Add ruamel_yaml compatibility shim

---
+++ ruamel_yaml.py
@@ -0,0 +1,11 @@
+import yaml as _pyyaml
+
+Loader = _pyyaml.Loader
+SafeLoader = _pyyaml.SafeLoader
+Dumper = _pyyaml.Dumper
+SafeDumper = _pyyaml.SafeDumper
+
+load = _pyyaml.load
+safe_load = _pyyaml.safe_load
+dump = _pyyaml.dump
+safe_dump = _pyyaml.safe_dump
